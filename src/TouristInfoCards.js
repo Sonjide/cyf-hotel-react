1adca226f9c4a93ea27c29011e1426b54c7e2418
Sonjide Hussain <sonjide.hussain@gmail.com>
2020-06-09 19:19:39 +0100

    fixed the img size in tourist card

--- src/TouristInfoCards.js
+++ src/TouristInfoCards.js
@@ -23,7 +23,7 @@ function TouristInfoCards() {
       {/* use of divs based on Lucy's grid.css*/}
 
       <div className="row">
-        <div className="col-8">
+        <div className="col-4">
           {/* Glasgow - Photo taken above Glasgow Queen Street train station. https://kraft.photo by Artur Kraft */}{" "}
           <div className="touristInfoCard">
             <img
@@ -40,16 +40,16 @@ function TouristInfoCards() {
             </div>
           </div>
         </div>
-        <div className="col-2">
+        <div className="col-4">
           {/* Manchester - Photo by William McCue */}
           <div className="touristInfoCard">
             <img
               src="https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2568&q=80"
               className="card-img-top"
-            />
+            />{" "}
             <div className="card-body">
               <a
-                href="https://www.visitmanchester.com"
+                href="https://peoplemakeglasgow.com"
                 className="btn btn-primary"
               >
                 Visit Manchester
@@ -57,61 +57,7 @@ function TouristInfoCards() {
             </div>
           </div>
         </div>
-        <div className="col-2">
-          {/* London - Tower Bridge - Photo by Charles Postiaux */}
-          <div className="touristInfoCard">
-            <img
-              src="https://images.unsplash.com/photo-1533929736458-ca588d08c8be?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <a
-                href="https://peoplemakeglasgow.com"
-                className="btn btn-primary"
-              >
-                Visit London
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="row">
-        <div className="col-8">
-          {/* Glasgow - Photo taken above Glasgow Queen Street train station. https://kraft.photo by Artur Kraft */}{" "}
-          <div className="touristInfoCard">
-            <img
-              src="https://images.unsplash.com/photo-1531152127291-ea24c3b2a1da?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2550&q=80"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <a
-                href="https://peoplemakeglasgow.com"
-                className="btn btn-primary"
-              >
-                Visit Glasgow
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-2">
-          {/* Manchester - Photo by William McCue */}
-          <div className="touristInfoCard">
-            <img
-              src="https://images.unsplash.com/photo-1515586838455-8f8f940d6853?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2568&q=80"
-              className="card-img-top"
-            />
-            <div className="card-body">
-              <a
-                href="https://www.visitmanchester.com"
-                className="btn btn-primary"
-              >
-                Visit Manchester
-              </a>
-            </div>
-          </div>
-        </div>
-        <div className="col-2">
+        <div className="col-4">
           {/* London - Tower Bridge - Photo by Charles Postiaux */}
           <div className="touristInfoCard">
             <img
